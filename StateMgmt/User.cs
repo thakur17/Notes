d50c2a3668843fb9126badefc5b9894a454c219d
thakur17 <42466300+thakur17@users.noreply.github.com>
2023-07-05 00:20:27 -0400

    Add files via upload

---
+++ StateMgmt/User.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication2.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+    }
+}
